d551d0bf67407cf081a558b0cf36ab9a50a41c68
Ivan Peresvetov <lumenist@mail.ru>
2024-05-13 17:52:16 +0300

    наброски по типам

--- src/components/VehicleFilters/FilterType/FilterType.tsx
+++ src/components/VehicleFilters/FilterType/FilterType.tsx
@@ -6,17 +6,42 @@ import { localize } from "../../../assets/dataArr"
 
 import styles from "./FilterType.module.scss"
 
-const FilterType: React.FC = () => {  
+const FilterType: React.FC = () => {
   const lang = useSelector(selectLang)
   const { type } = localize
 
   return (
     <fieldset className={styles.filter__type}>
       <legend>{type[lang]}</legend>
-      <ul>
-        {/* <li>Танк</li>
-      <li>Самолёт</li> */}
-      </ul>
+      <table>
+        <tr>
+          <td>Армия</td>
+          <td>
+            <p>Лёгкий танк</p>
+            <p>Средний танк</p>
+            <p>Тяжёлый танк</p>
+            <p>САУ</p>
+          </td>
+        </tr>
+        <tr>
+          <td>Авиация</td>
+          <td>
+            <p>Истребитель</p>
+            <p>Штурмовик</p>
+            <p>Бомбордировщик</p>
+          </td>
+        </tr>
+        <tr>
+        <td>Флот</td>
+          <td>
+            <p>Никому не нужен</p>
+            <p>Ты что, флотоводец?</p>
+          </td>
+        </tr>
+        <tr>
+          Эвенты, ядерка
+        </tr>
+      </table>
     </fieldset>
   )
 }
